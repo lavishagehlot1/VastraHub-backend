62c44f6facf8d288c0224c4739533b4d57eb6340
lavisha <lavishagehlot1@gmail.com>
2026-04-21 10:38:17 +0530

    created sellerProfile submitForReview approveSeller rejectSeller api's

--- src/routes/sellerRoute/sellerRoute.js
+++ src/routes/sellerRoute/sellerRoute.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { authorization } from '../../middleware/authorization.js';
-import { applySeller, sellerProfile } from '../../controller/sellerController/sellerController.js';
+import { applySeller, approveSeller, rejectSeller, sellerProfile, submitForReview } from '../../controller/sellerController/sellerController.js';
 import { authorize } from '../../middleware/authorizeRole.js';
+import { validation } from '../../middleware/validations.js';
+import { sellerProfileSchema } from '../../validations/sellerValidation.js';
+import { seller } from '../../model/seller/sellerModel.js';
 const sellerRoute=express.Router();
 
 sellerRoute.use(authorization);
@@ -11,7 +14,24 @@ sellerRoute.post('/applySeller',
 );
 
 sellerRoute.post('/sellerProfile',
+  validation({body:sellerProfileSchema}),  
     authorize('user'),
     sellerProfile
 )
-export default sellerRoute;
+
+sellerRoute.post('/submitForReview',
+    validation({body:sellerProfileSchema}),
+    authorize('user'),
+    submitForReview
+);
+
+sellerRoute.post('/approveSeller/:id',
+    authorize('admin'),
+    approveSeller
+)
+
+sellerRoute.post('/rejectSeller/:id',
+    authorize('admin'),
+    rejectSeller
+)
+export default sellerRoute; 
